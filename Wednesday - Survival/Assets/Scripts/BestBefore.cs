83abf0e185f08fb08e763e18e9cb3e248de3cb40
Rymdlingen <71494058+Rymdlingen@users.noreply.github.com>
2021-06-02 15:14:19 +0200

    Day 3 of 1-hour game jam completed.

---
+++ Wednesday - Survival/Assets/Scripts/BestBefore.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BestBefore : MonoBehaviour
+{
+    public float goodWithinThisTime = 2f;
+    float timer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = goodWithinThisTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= Time.deltaTime;
+
+        if (timer < 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
